22924022741cf003721515d069de62270279ab9f
Damodar Lohani <dlohani48@gmail.com>
2020-03-21 12:25:46 +0545

    refactoring into library

---
+++ lib/src/days_view.dart
@@ -0,0 +1,188 @@
+part of clean_nepali_calendar;
+
+const double _kDayPickerRowHeight = 42.0;
+
+class _DayPickerGridDelegate extends SliverGridDelegate {
+  const _DayPickerGridDelegate();
+
+  @override
+  SliverGridLayout getLayout(SliverConstraints constraints) {
+    const columnCount = 7;
+    final tileWidth = constraints.crossAxisExtent / columnCount;
+    final tileHeight = math.min(_kDayPickerRowHeight,
+        constraints.viewportMainAxisExtent / (_kMaxDayPickerRowCount + 1));
+    return SliverGridRegularTileLayout(
+      crossAxisCount: columnCount,
+      mainAxisStride: tileHeight,
+      crossAxisStride: tileWidth,
+      childMainAxisExtent: tileHeight,
+      childCrossAxisExtent: tileWidth,
+      reverseCrossAxis: axisDirectionIsReversed(constraints.crossAxisDirection),
+    );
+  }
+
+  @override
+  bool shouldRelayout(_DayPickerGridDelegate oldDelegate) => false;
+}
+
+const _DayPickerGridDelegate _kDayPickerGridDelegate = _DayPickerGridDelegate();
+
+class DaysView extends StatelessWidget {
+  DaysView({
+    Key key,
+    @required this.selectedDate,
+    @required this.currentDate,
+    @required this.onChanged,
+    @required this.firstDate,
+    @required this.lastDate,
+    @required this.displayedMonth,
+    @required this.language,
+    this.selectableDayPredicate,
+    this.dragStartBehavior = DragStartBehavior.start,
+  })  : assert(selectedDate != null),
+        assert(currentDate != null),
+        assert(onChanged != null),
+        assert(displayedMonth != null),
+        assert(dragStartBehavior != null),
+        assert(!firstDate.isAfter(lastDate)),
+        assert(selectedDate.isAfter(firstDate)),
+        super(key: key);
+
+  final NepaliDateTime selectedDate;
+
+  final NepaliDateTime currentDate;
+
+  final ValueChanged<NepaliDateTime> onChanged;
+
+  final NepaliDateTime firstDate;
+
+  final NepaliDateTime lastDate;
+
+  final NepaliDateTime displayedMonth;
+
+  final SelectableDayPredicate selectableDayPredicate;
+
+  final DragStartBehavior dragStartBehavior;
+
+  final Language language;
+
+  List<Widget> _getDayHeaders(Language language, TextStyle headerStyle) {
+    return (language == Language.english
+            ? ['S', 'M', 'T', 'W', 'T', 'F', 'S']
+            : ['आ', 'सो', 'मं', 'बु', 'वि', 'शु', 'श'])
+        .map(
+          (label) => ExcludeSemantics(
+            child: Center(
+              child: Text(label, style: headerStyle),
+            ),
+          ),
+        )
+        .toList();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final themeData = Theme.of(context);
+    final year = displayedMonth.year;
+    final month = displayedMonth.month;
+    final daysInMonth = displayedMonth.totalDays;
+    final firstDayOffset = displayedMonth.weekDay - 1;
+    final labels = <Widget>[];
+    labels.addAll(
+      _getDayHeaders(language, themeData.textTheme.caption),
+    );
+    for (var i = 0; true; i += 1) {
+      // 1-based day of month, e.g. 1-31 for January, and 1-29 for February on
+      // a leap year.
+      final day = i - firstDayOffset + 1;
+      if (day > daysInMonth) break;
+      if (day < 1) {
+        labels.add(Container());
+      } else {
+        final dayToBuild = NepaliDateTime(year, month, day);
+        final disabled = dayToBuild.isAfter(lastDate) ||
+            dayToBuild.isBefore(firstDate) ||
+            (selectableDayPredicate != null &&
+                !selectableDayPredicate(dayToBuild));
+
+        BoxDecoration decoration;
+        var itemStyle = themeData.textTheme.body1;
+
+        final isSelectedDay = selectedDate.year == year &&
+            selectedDate.month == month &&
+            selectedDate.day == day;
+        if (isSelectedDay) {
+          // The selected day gets a circle background highlight, and a contrasting text color.
+          itemStyle = themeData.accentTextTheme.body2;
+          decoration = BoxDecoration(
+            color: themeData.accentColor,
+            shape: BoxShape.circle,
+          );
+        } else if (disabled) {
+          itemStyle = themeData.textTheme.body1
+              .copyWith(color: themeData.disabledColor);
+        } else if (currentDate.year == year &&
+            currentDate.month == month &&
+            currentDate.day == day) {
+          // The current day gets a different text color.
+          itemStyle =
+              themeData.textTheme.body2.copyWith(color: themeData.accentColor);
+        }
+
+        Widget dayWidget = Container(
+          decoration: decoration,
+          child: Center(
+            child: Semantics(
+              label: '${formattedMonth(month, Language.english)} $day, $year',
+              selected: isSelectedDay,
+              child: ExcludeSemantics(
+                child: Text(
+                    '${language == Language.english ? day : NepaliUnicode.convert('$day')}',
+                    style: itemStyle),
+              ),
+            ),
+          ),
+        );
+
+        if (!disabled) {
+          dayWidget = GestureDetector(
+            behavior: HitTestBehavior.opaque,
+            onTap: () {
+              onChanged(dayToBuild);
+            },
+            child: dayWidget,
+            dragStartBehavior: dragStartBehavior,
+          );
+        }
+
+        labels.add(dayWidget);
+      }
+    }
+
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 8.0),
+      child: Column(
+        children: <Widget>[
+          Container(
+            height: _kDayPickerRowHeight,
+            child: Center(
+              child: ExcludeSemantics(
+                child: Text(
+                  '${formattedMonth(month, language)} ${language == Language.english ? year : NepaliUnicode.convert('$year')}',
+                  style: themeData.textTheme.subhead,
+                ),
+              ),
+            ),
+          ),
+          Flexible(
+            child: GridView.custom(
+              gridDelegate: _kDayPickerGridDelegate,
+              childrenDelegate:
+                  SliverChildListDelegate(labels, addRepaintBoundaries: false),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/src/month_view.dart
@@ -0,0 +1,271 @@
+part of clean_nepali_calendar;
+
+const Duration _kMonthScrollDuration = Duration(milliseconds: 200);
+
+class MonthView extends StatefulWidget {
+  MonthView({
+    Key key,
+    @required this.selectedDate,
+    @required this.onChanged,
+    @required this.firstDate,
+    @required this.lastDate,
+    @required this.language,
+    this.selectableDayPredicate,
+    this.dragStartBehavior = DragStartBehavior.start,
+  })  : assert(selectedDate != null),
+        assert(onChanged != null),
+        assert(!firstDate.isAfter(lastDate)),
+        assert(selectedDate.isAfter(firstDate)),
+        super(key: key);
+
+  final NepaliDateTime selectedDate;
+
+  final ValueChanged<NepaliDateTime> onChanged;
+
+  final NepaliDateTime firstDate;
+
+  final NepaliDateTime lastDate;
+
+  final SelectableDayPredicate selectableDayPredicate;
+
+  final DragStartBehavior dragStartBehavior;
+
+  final Language language;
+
+  @override
+  _MonthViewState createState() => _MonthViewState();
+}
+
+class _MonthViewState extends State<MonthView>
+    with SingleTickerProviderStateMixin {
+  static final Animatable<double> _chevronOpacityTween =
+      Tween<double>(begin: 1.0, end: 0.0)
+          .chain(CurveTween(curve: Curves.easeInOut));
+
+  @override
+  void initState() {
+    super.initState();
+    // Initially display the pre-selected date.
+    final monthPage = _monthDelta(widget.firstDate, widget.selectedDate);
+    _dayPickerController = PageController(initialPage: monthPage);
+    _handleMonthPageChanged(monthPage);
+    _updateCurrentDate();
+
+    // Setup the fade animation for chevrons
+    _chevronOpacityController = AnimationController(
+      duration: const Duration(milliseconds: 250),
+      vsync: this,
+    );
+    _chevronOpacityAnimation =
+        _chevronOpacityController.drive(_chevronOpacityTween);
+  }
+
+  @override
+  void didUpdateWidget(MonthView oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    if (widget.selectedDate != oldWidget.selectedDate) {
+      final monthPage = _monthDelta(widget.firstDate, widget.selectedDate);
+      _dayPickerController = PageController(initialPage: monthPage);
+      _handleMonthPageChanged(monthPage);
+    }
+  }
+
+  TextDirection textDirection;
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    textDirection = Directionality.of(context);
+  }
+
+  NepaliDateTime _todayDate;
+  NepaliDateTime _currentDisplayedMonthDate;
+  Timer _timer;
+  PageController _dayPickerController;
+  AnimationController _chevronOpacityController;
+  Animation<double> _chevronOpacityAnimation;
+
+  void _updateCurrentDate() {
+    _todayDate = NepaliDateTime.now();
+    final tomorrow = NepaliDateTime(
+      _todayDate.year,
+      _todayDate.month,
+      _todayDate.day + 1,
+    );
+    var timeUntilTomorrow = tomorrow.difference(_todayDate);
+    timeUntilTomorrow +=
+        const Duration(seconds: 1); // so we don't miss it by rounding
+    _timer?.cancel();
+    _timer = Timer(timeUntilTomorrow, () {
+      setState(_updateCurrentDate);
+    });
+  }
+
+  static int _monthDelta(NepaliDateTime startDate, NepaliDateTime endDate) {
+    return (endDate.year - startDate.year) * 12 +
+        endDate.month -
+        startDate.month;
+  }
+
+  NepaliDateTime _addMonthsToMonthDate(
+    NepaliDateTime monthDate,
+    int monthsToAdd,
+  ) {
+    int year = monthsToAdd ~/ 12;
+    int months = monthDate.month + monthsToAdd % 12;
+    if (months > 12) {
+      year += months ~/ 12;
+      months = months % 12;
+    }
+    return NepaliDateTime(
+      monthDate.year + year,
+      months,
+    );
+  }
+
+  Widget _buildItems(BuildContext context, int index) {
+    final month = _addMonthsToMonthDate(widget.firstDate, index);
+    return DaysView(
+      key: ValueKey<NepaliDateTime>(month),
+      selectedDate: widget.selectedDate,
+      currentDate: _todayDate,
+      onChanged: widget.onChanged,
+      firstDate: widget.firstDate,
+      lastDate: widget.lastDate,
+      displayedMonth: month,
+      language: widget.language,
+      selectableDayPredicate: widget.selectableDayPredicate,
+      dragStartBehavior: widget.dragStartBehavior,
+    );
+  }
+
+  void _handleNextMonth() {
+    if (!_isDisplayingLastMonth) {
+      SemanticsService.announce(
+          "${formattedMonth(_nextMonthDate.month, Language.english)} ${_nextMonthDate.year}",
+          textDirection);
+      _dayPickerController.nextPage(
+          duration: _kMonthScrollDuration, curve: Curves.ease);
+    }
+  }
+
+  void _handlePreviousMonth() {
+    if (!_isDisplayingFirstMonth) {
+      SemanticsService.announce(
+          "${formattedMonth(_previousMonthDate.month, Language.english)} ${_previousMonthDate.year}",
+          textDirection);
+      _dayPickerController.previousPage(
+          duration: _kMonthScrollDuration, curve: Curves.ease);
+    }
+  }
+
+  bool get _isDisplayingFirstMonth {
+    return !_currentDisplayedMonthDate
+        .isAfter(NepaliDateTime(widget.firstDate.year, widget.firstDate.month));
+  }
+
+  bool get _isDisplayingLastMonth {
+    return !_currentDisplayedMonthDate
+        .isBefore(NepaliDateTime(widget.lastDate.year, widget.lastDate.month));
+  }
+
+  NepaliDateTime _previousMonthDate;
+  NepaliDateTime _nextMonthDate;
+
+  void _handleMonthPageChanged(int monthPage) {
+    setState(() {
+      _previousMonthDate =
+          _addMonthsToMonthDate(widget.firstDate, monthPage - 1);
+      _currentDisplayedMonthDate =
+          _addMonthsToMonthDate(widget.firstDate, monthPage);
+      _nextMonthDate = _addMonthsToMonthDate(widget.firstDate, monthPage + 1);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: _kMaxDayPickerHeight,
+      child: Stack(
+        children: <Widget>[
+          Semantics(
+            sortKey: _MonthPickerSortKey.calendar,
+            child: NotificationListener<ScrollStartNotification>(
+              onNotification: (_) {
+                _chevronOpacityController.forward();
+                return false;
+              },
+              child: NotificationListener<ScrollEndNotification>(
+                onNotification: (_) {
+                  _chevronOpacityController.reverse();
+                  return false;
+                },
+                child: PageView.builder(
+                  dragStartBehavior: widget.dragStartBehavior,
+                  key: ValueKey<NepaliDateTime>(widget.selectedDate),
+                  controller: _dayPickerController,
+                  scrollDirection: Axis.horizontal,
+                  itemCount: _monthDelta(widget.firstDate, widget.lastDate) + 1,
+                  itemBuilder: _buildItems,
+                  onPageChanged: _handleMonthPageChanged,
+                ),
+              ),
+            ),
+          ),
+          PositionedDirectional(
+            top: 0.0,
+            start: 8.0,
+            child: Semantics(
+              sortKey: _MonthPickerSortKey.previousMonth,
+              child: FadeTransition(
+                opacity: _chevronOpacityAnimation,
+                child: IconButton(
+                  icon: const Icon(Icons.chevron_left),
+                  tooltip: _isDisplayingFirstMonth
+                      ? null
+                      : 'Previous month ${formattedMonth(_previousMonthDate.month, Language.english)} ${_previousMonthDate.year}',
+                  onPressed:
+                      _isDisplayingFirstMonth ? null : _handlePreviousMonth,
+                ),
+              ),
+            ),
+          ),
+          PositionedDirectional(
+            top: 0.0,
+            end: 8.0,
+            child: Semantics(
+              sortKey: _MonthPickerSortKey.nextMonth,
+              child: FadeTransition(
+                opacity: _chevronOpacityAnimation,
+                child: IconButton(
+                  icon: const Icon(Icons.chevron_right),
+                  tooltip: _isDisplayingLastMonth
+                      ? null
+                      : 'Next month ${formattedMonth(_nextMonthDate.month, Language.english)} ${_nextMonthDate.year}',
+                  onPressed: _isDisplayingLastMonth ? null : _handleNextMonth,
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    _timer?.cancel();
+    _dayPickerController?.dispose();
+    super.dispose();
+  }
+}
+
+// Defines semantic traversal order of the top-level widgets inside the month
+// picker.
+class _MonthPickerSortKey extends OrdinalSortKey {
+  const _MonthPickerSortKey(double order) : super(order);
+
+  static const _MonthPickerSortKey previousMonth = _MonthPickerSortKey(1.0);
+  static const _MonthPickerSortKey nextMonth = _MonthPickerSortKey(2.0);
+  static const _MonthPickerSortKey calendar = _MonthPickerSortKey(3.0);
+}
